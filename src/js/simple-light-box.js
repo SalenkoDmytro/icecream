45a51661b54290ba64d732e53dd4133d66afe3cf
Maksym-Holovachuk <mgolovachuk@gmail.com>
2022-07-04 17:37:43 +0300

    add js-file

---
+++ src/js/simple-light-box.js
@@ -0,0 +1,10 @@
+let gallery = new SimpleLightbox('.gallery a',{captionDelay:300});
+
+
+// gallery.on('show.simplelightbox', function () {
+// 	// do something…
+// });
+
+// gallery.on('error.simplelightbox', function (e) {
+// 	console.log(e); // some usefull information
+// });
