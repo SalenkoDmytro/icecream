80adec3541045df6aff8463c128c46cd2ae77c65
Maksym-Holovachuk <mgolovachuk@gmail.com>
2022-07-04 19:59:45 +0300

    js added

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,15 +1,4 @@
 (() => {
-  document
-    .querySelector('.js-speaker-form')
-    .addEventListener('submit', e => {
-      e.preventDefault();
-      new FormData(e.currentTarget).forEach((value, name) =>
-        console.log(`${name}: ${value}`));
-    });
-})();
-
-
-(() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
