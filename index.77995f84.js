74c19d8def9dc0c2fb05ed7716cf0c378f37dc7b
SalenkoDmytro <105796636+SalenkoDmytro@users.noreply.github.com>
2022-10-25 17:32:18 +0000

    Deploying to gh-pages from @ SalenkoDmytro/icecream@65f0b92b6572717ab8ef4e362f03650e5b1e92b0 🚀

---
+++ index.77995f84.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[data-mob-modal-open]"),modal:document.querySelector("[data-modal]")};o.openModalBtn.addEventListener("click",(function(){o.modal.classList.toggle("is-hidden")}))})();
+//# sourceMappingURL=index.77995f84.js.map
---
+++ index.dfecce1f.js
@@ -0,0 +1,2 @@
+!function(){var o;(o={openModalBtn:document.querySelector("[data-mob-modal-open]"),modal:document.querySelector("[data-modal]")}).openModalBtn.addEventListener("click",(function(){o.modal.classList.toggle("is-hidden")}))}();
+//# sourceMappingURL=index.dfecce1f.js.map
