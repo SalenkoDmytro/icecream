65f0b92b6572717ab8ef4e362f03650e5b1e92b0
SalenkoDmytro <dmytrosalenko1996@gmail.com>
2022-10-25 20:31:43 +0300

    modalFix

--- src/js/mob-modal.js
+++ src/js/mob-modal.js
@@ -1,12 +1,12 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-mob-modal-open]'),
-    closeModalBtn: document.querySelector('[data-mob-modal-close]'),
+    // closeModalBtn: document.querySelector('[data-mob-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  // refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
